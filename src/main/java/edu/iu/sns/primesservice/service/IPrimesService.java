85c171ffd40905754ce9f25f11c5551a64659532
jayasandeepketha <159554759+jayasandeepketha@users.noreply.github.com>
2024-02-22 17:54:18 -0500

    Add files via upload

---
+++ src/main/java/edu/iu/sns/primesservice/service/IPrimesService.java
@@ -0,0 +1,5 @@
+package edu.iu.sns.primesservice.service;
+
+public interface IPrimesService {
+    boolean isPrime(long n);
+}
---
+++ src/test/java/edu/iu/sns/primesservice/service/PrimesServiceTest.java
@@ -0,0 +1,17 @@
+package edu.iu.sns.primesservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimesServiceTest {
+    PrimesService primesService = new PrimesService();
+    @Test
+    void _45IsNotPrime(){
+        int n = 45;
+        boolean expected = false;
+        boolean actual = primesService.isPrime(n);
+        assertEquals(expected, actual);
+    }
+
+}
