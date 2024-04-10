0439d68df808d405b722cea990196cc941122597
jayasandeepketha <159554759+jayasandeepketha@users.noreply.github.com>
2024-04-09 21:54:03 -0400

    Update AuthenticationController.java

--- src/main/java/com/iu/rokala/primesservice/controller/AuthenticationController.java
+++ src/main/java/com/iu/rokala/primesservice/controller/AuthenticationController.java
@@ -15,6 +15,7 @@ import com.iu.rokala.primesservice.service.IAuthenticationService;
 import com.iu.rokala.primesservice.service.TokenService;
 
 @RestController
+@CrossOrigin({"http://127.0.0.1:5500"})
 public class AuthenticationController {
 
 	private IAuthenticationService authenticationService;
