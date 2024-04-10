2acf23bb8a6be81a9d729950528b2401df3e46e6
jayasandeepketha <159554759+jayasandeepketha@users.noreply.github.com>
2024-04-09 21:55:50 -0400

    Update PrimesController.java

--- src/main/java/com/iu/rokala/primesservice/controller/PrimesController.java
+++ src/main/java/com/iu/rokala/primesservice/controller/PrimesController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.iu.rokala.primesservice.service.IPrimesService;
+import com.iu.rokala.primesservice.rabbitmq.MQSender;
 
 @RestController
 @CrossOrigin
